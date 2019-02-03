9408e06324c425bb0cee4dcabd3fb478af0d3a46
pawelkleczek <pa.vel.kleczek@gmail.com>
2019-02-03 15:27:30 +0100

    Initial commit

---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+  @Get()
+  
+  findAll() {
+    return 'This action returns all cats';
+  }
+}
